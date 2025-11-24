6924e00f38a4423cd6ddcf0ebcb88d5edcbb2e21
Guest User <Guest@BP-36-201-16s-iMac.local>
2025-11-24 10:42:55 +0300

    start

---
+++ CampusCare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusCare
+//
+//  Created by Guest User on 24/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
