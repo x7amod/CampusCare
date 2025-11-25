6d7518c782b72cc9a3ff6cf1da3d5cbffce42138
Ahmed <thebeastbhr@gmail.com>
2025-11-25 15:02:27 +0300

    create firebase connection

--- CampusCare/ViewController.swift
+++ CampusCare/ViewController.swift
@@ -7,7 +7,39 @@
 
 import UIKit
 import FirebaseFirestore
+import Cloudinary
 
 class ViewController: UIViewController {
-    
+
+    let cloudName: String = "ducxhdkkd"
+    var uploadPreset: String = "student_upload_preset" //NEW - Name of unsigned upload preset
+
+    @IBOutlet weak var ivUploadedImage: CLDUIImageView! //NEW - Outlet for uploaded image
+
+    var cloudinary: CLDCloudinary!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initCloudinary()
+        uploadImage() //NEW - Call upload function
+    }
+    private func initCloudinary() {
+        let config = CLDConfiguration(cloudName: cloudName, secure: true)
+        cloudinary = CLDCloudinary(configuration: config)
+    }
+    //NEW - Upload function
+    private func uploadImage() {
+        guard let data = UIImage(named: "cloudinary_logo")?.pngData() else {
+            return
+        }
+        cloudinary.createUploader().upload(data: data, uploadPreset: uploadPreset, completionHandler:  { response, error in
+            DispatchQueue.main.async {
+                guard let url = response?.secureUrl else {
+                    return
+                }
+                self.ivUploadedImage.cldSetImage(url, cloudinary: self.cloudinary)
+            }
+        })
+    }
+
 }
