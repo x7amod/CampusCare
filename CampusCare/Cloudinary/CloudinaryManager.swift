b7f1ae9dab36325a294cc842596970971e1c6f59
rentamac <rentamac@Mac-mini-de-rentamac.local>
2025-12-30 19:29:26 -0800

    done with cloudinry connect to upload photos + did some simple design changes

--- CampusCare/Cloudinary/CloudinaryManager.swift
+++ CampusCare/Cloudinary/CloudinaryManager.swift
@@ -22,7 +22,7 @@ class CloudinaryManager {
         let config = CLDConfiguration(cloudName: cloudName, secure: true)
         cloudinary = CLDCloudinary(configuration: config)
     }
-
+// Upload PDF
     func uploadPDF(
         _ pdfData: Data,
         fileName: String,
@@ -37,7 +37,8 @@ class CloudinaryManager {
             data: pdfData,
             uploadPreset: uploadPreset,
             params: params
-        ) { response, error in
+        )
+        { response, error in
 
             if let urlString = response?.secureUrl,
                let url = URL(string: urlString) {
@@ -49,5 +50,39 @@ class CloudinaryManager {
             }
         }
     }
+    
+    
+    //Upload Image
+    func uploadImage(
+        image: UIImage,
+        completion: @escaping (String?) -> Void
+    ) {
+        guard let data = image.jpegData(compressionQuality: 0.8) else {
+            completion(nil)
+            return
+        }
+
+        let params = CLDUploadRequestParams()
+        params.setFolder("request_images")   // images folder
+
+        cloudinary.createUploader().upload(
+            data: data,
+            uploadPreset: uploadPreset,
+            params: params
+        ) { response, error in
+
+            if let url = response?.secureUrl {
+                completion(url)
+            } else {
+                print("Cloudinary image upload error:",
+                      error?.localizedDescription ?? "Unknown error")
+                completion(nil)
+            }
+        }
+    }
+
+    
 }
+
+
 
